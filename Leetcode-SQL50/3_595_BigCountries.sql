5fd606ae116119fb7bbb9c3105259cc2375fe7af
AmishaSomaiya <somaiyaamisha2@gmail.com>
2024-04-17 08:31:09 -0700

    Leetcode SQL50

---
+++ Leetcode-SQL50/3_595_BigCountries.sql
@@ -0,0 +1,31 @@
+/*
+Table: World
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key (column with unique values) for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+ 
+
+A country is big if:
+
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+
+Return the result table in any order.
+
+EASY
+*/
+
+SELECT name, population, area 
+FROM World
+WHERE area >= 3000000
+OR population >= 25000000;
